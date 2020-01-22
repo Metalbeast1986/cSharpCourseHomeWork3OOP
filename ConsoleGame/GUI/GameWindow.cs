248b8d74675fd4bfdd5d81ef7bbd575feeb2b2bc
ksistof1986 <ksistof1986@gmail.com>
2020-01-22 15:18:47 +0200

    keyboard input in progress

--- ConsoleGame/GUI/GameWindow.cs
+++ ConsoleGame/GUI/GameWindow.cs
@@ -10,19 +10,25 @@ namespace ConsoleGame.GUI
         private Button _creditsButton;
         private Button _quitButton;
         private TextBlock _titleTextBlock;
-
+        public List<Button> buttonList = new List<Button>();
 
         public GameWindow() : base(0, 0, 120, 30, '%')
         {
             _titleTextBlock = new TextBlock(10, 5, 100, new List<String> { "Call of Duty", "Vardas Pavardaitis kuryba!", "Made in Vilnius Coding School!" });
 
             _startButton = new Button(20, 13, 18, 5, "Start");
-            _startButton.SetActive();
+            //_startButton.SetActive();
 
             _creditsButton = new Button(50, 13, 18, 5, "Credits");
 
             _quitButton = new Button(80, 13, 18, 5, "Quit");
+
+            buttonList.Add(_startButton);
+            buttonList.Add(_creditsButton);
+            buttonList.Add(_quitButton);
+
         }
+       
 
         public override void Render()
         {
--- ConsoleGame/GUI/GuiController.cs
+++ ConsoleGame/GUI/GuiController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleGame.GUI
 {
@@ -9,18 +11,98 @@ namespace ConsoleGame.GUI
         private GameWindow gameWindow;
         private CreditWindow creditWindow;
 
+        int index = 0;
+        ConsoleKey key;
+
         public GuiController()
         {
             gameWindow = new GameWindow();
-
             creditWindow = new CreditWindow();
 
         }
        public void ShowMenu()
        {
-            gameWindow.Render();
-            creditWindow.Render();
+            //Ar ShowMenu funkcijoi zemiau einantys elementai reikalingi?
+           gameWindow.Render();
+            //creditWindow.Render();
+
+            //call out Select function to display all possible buttons
+            Active(Select(gameWindow.buttonList));
        }
+        void Active(int index)
+        {
+            gameWindow.buttonList[index].SetActive();
+        }
+        int Select(List<Button> buttonList)
+        {
+           
+            //get all list indexes
+            for (int i=0; i <= buttonList.Count; i++)
+            {
+
+                if (i == index)
+                {
+
+                }
+
+
+
+                    Console.WriteLine("button display "+i);
+            }
+
+            key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    {/*
+                        if (index > 0 && index <= buttonList.Count)
+                        {*/
+                        Console.WriteLine("left");
+                            index -= index;
+                        //}
+                        break;
+                    }
+                case ConsoleKey.RightArrow:
+                    {/*
+                        if(index > 0 && index < buttonList.Count)
+                        {*/
+                        Console.WriteLine("right");
+                            index += index;
+                       // }
+                        break;
+                    }
+            }
+
+            /*
+            //on key press change index. by calling function
+            if (ConsoleKey.LeftArrow) // if left arrow
+            {
+                if (index > 0 && index < buttonList.Count)
+                {
+                    index += index;
+                }
+
+                MessageBox.Show("Enter Key Pressed ");
+            }
+            if (e.KeyCode == Keys.Enter) // if right arrow
+            {
+                if (index > 0 && index <= buttonList.Count)
+                {
+
+                    index -= index;
+                }
+
+                MessageBox.Show("Enter Key Pressed ");
+            }
+            */
+            
+
+
+            return index;
+            //gameWindow.buttonList[index]
+        }
+
         
     }
 }
